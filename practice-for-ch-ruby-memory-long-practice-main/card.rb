f6ad98508c0a9bfb6ab24eaf6c8473bff8848dd9
Yaqi Fang <cfang322@gmail.com>
2023-10-31 13:58:41 -0400

    card class

--- practice-for-ch-ruby-memory-long-practice-main/card.rb
+++ practice-for-ch-ruby-memory-long-practice-main/card.rb
@@ -1,5 +1,28 @@
 class Card
-  def initialize
 
+  def initialize(value)
+    @value = value
+    @face_up = false
   end
+
+  def hide
+    @face_up = false
+  end
+
+  def reveal?
+    @face_up = true
+  end
+
+  def value
+    @value if @face_up == true
+  end
+
+  def to_s
+    @value.to_s 
+  end
+
+  def ==(card)
+    @value == card.value 
+  end
+
 end
